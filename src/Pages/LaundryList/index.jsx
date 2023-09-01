1cdbbf13867047511668fb6d9ebcd036730fdbd2
maureen-treacy <mtreacy07@gmail.com>
2023-09-01 16:15:21 +0100

    add to laundry list

--- src/Pages/LaundryList/index.jsx
+++ src/Pages/LaundryList/index.jsx
@@ -1,6 +1,8 @@
-{/*display added to laundry list */}
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+{
+  /*display added to laundry list */
+}
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const API_URL = "http://localhost:5005";
 
@@ -10,12 +12,19 @@ function LaundryList() {
   useEffect(() => {
     async function fetchLaundry() {
       try {
-        const response = await axios.get('/api/laundry');
+        const storedToken = localStorage.getItem("authToken");
+        const response = await axios.get(`${API_URL}/api/laundry`, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        });
+        setLaundry(response.data.laundry);
+        /* const response = await axios.get("/api/laundry");
         if (response.status === 200) {
           setLaundry(response.data);
-        }
+        } */
       } catch (error) {
-        console.error('Error getting laundry:', error);
+        console.error("Error getting laundry:", error);
       }
     }
 
@@ -26,14 +35,10 @@ function LaundryList() {
     <div>
       <h2>Laundry List</h2>
       <ul>
-        {laundry.map(item => (
-          <li key={item._id}>{item.name}</li>
-        ))}
+        {laundry && laundry.map((item) => <li key={item._id}>{item.title}</li>)}
       </ul>
     </div>
   );
 }
 
 export default LaundryList;
-
-
