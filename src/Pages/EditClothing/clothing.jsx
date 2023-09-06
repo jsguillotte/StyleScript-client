3951a035016c134caf364e088464775e61c79c99
maureen-treacy <mtreacy07@gmail.com>
2023-09-06 16:37:21 +0100

    API url added

--- src/Pages/EditClothing/clothing.jsx
+++ src/Pages/EditClothing/clothing.jsx
@@ -3,7 +3,7 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 import {useParams, useNavigate} from 'react-router-dom';
 
-const API_URL = 'http://localhost:5005';
+const API_URL = 'https://style-script.onrender.com"';
 
 function EditClothing() {
 
