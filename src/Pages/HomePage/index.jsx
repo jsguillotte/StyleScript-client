bfa7b413c58566f397d163e9c5aef1c3fa63739b
jsguillotte <julia.guillotte@gmail.com>
2023-09-08 03:44:43 -0500

    home page

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -9,7 +9,7 @@ function HomePage() {
     <div className="home-hero">
       <div className="hero-text">
         
-      <Link to="/login"><h1>Personal Closet Planner</h1></Link>
+      <h1>Personal Closet Planner</h1>
       </div>
     </div>
      
