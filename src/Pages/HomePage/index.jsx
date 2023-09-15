bc1105ad6612f2539c9316bd570198253c76c047
jsguillotte <julia.guillotte@gmail.com>
2023-09-15 07:42:58 -0500

    home page img path

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -20,15 +20,15 @@ function HomePage() {
       <div>
         <article>
           <h3>Organize your clothes</h3>
-          <img src="../../public/images/Folded-clothing.jpg" alt="" width={300}/>
+          <img src="/images/Folded-clothing.jpg" alt="" width={300}/>
         </article>
         <article>
           <h3>Keep track of laundry</h3>
-          <img src="../../public/images/laundry.jpg" alt="" width={300}/>
+          <img src="/images/laundry.jpg" alt="" width={300}/>
         </article>
         <article>
           <h3>Create your packing list</h3>
-          <img src="../../public/images/packing.jpg" alt="" width={300}/>
+          <img src="/images/packing.jpg" alt="" width={300}/>
         </article>
         
       </div>
@@ -38,8 +38,8 @@ function HomePage() {
       <div >
         <div className="footer-text">
           
-        <p> Julia Guillotte <a href="https://www.linkedin.com/in/julia-guillotte" target="_blank" rel="noopener noreferrer"><img src="/../../public/images/linkedin.png" height="15px" width="15px" alt=""/></a><a href="https://github.com/jsguillotte" target="_blank" rel="noopener noreferrer"><img src="../public/images/github.png" height="15px" width="15px" alt=""/></a>
-        | Maureen Treacy <a href="https://www.linkedin.com/in/maureen-treacy-30714190/" target="_blank" rel="noopener noreferrer"><img src="/images/linkedin.png" height="15px" width="15px" alt=""/></a><a href="https://github.com/maureen-treacy" target="_blank" rel="noopener noreferrer"><img src="../public/images/github.png" height="15px" width="15px" alt=""/></a></p>
+        <p> Julia Guillotte <a href="https://www.linkedin.com/in/julia-guillotte" target="_blank" rel="noopener noreferrer"><img src="/images/linkedin.png" height="15px" width="15px" alt=""/></a><a href="https://github.com/jsguillotte" target="_blank" rel="noopener noreferrer"><img src="/images/github.png" height="15px" width="15px" alt=""/></a>
+        | Maureen Treacy <a href="https://www.linkedin.com/in/maureen-treacy-30714190/" target="_blank" rel="noopener noreferrer"><img src="/images/linkedin.png" height="15px" width="15px" alt=""/></a><a href="https://github.com/maureen-treacy" target="_blank" rel="noopener noreferrer"><img src="/images/github.png" height="15px" width="15px" alt=""/></a></p>
         </div>
       </div>
 
